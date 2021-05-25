de03adfb44c7016d7149634128888d6f8c6797cc
Jacob Schneck <jschneck@Gerald.localdomain>
2021-05-24 19:58:47 -0500

    API call finished -tentatively

--- my-app/src/index.tsx
+++ my-app/src/index.tsx
@@ -1,101 +1,124 @@
 // @ts-nocheck
+
 import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
+import { resourceLimits } from 'worker_threads';
+// import {GetUserData} from './services.tsx';
 import './index.css';
 
 
 /********************************************** API CALLS **************************************************/
-const urlUser: string = 'https://aoe2.net/api/player/matches?game=aoe2de&steam_id=76561198134218675&count=1'
-
-function GetUserData() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [data, setData] = useState([]);
+const urlUser = 'https://aoe2.net/api/player/matches?game=aoe2de&steam_id=76561198134218675&count=1'
 
 
-  useEffect( () => {
-    fetch('https://aoe2.net/api/player/matches?game=aoe2de&steam_id=76561198134218675&count=1')
-      .then(res => res.json())
-      .then( (result) => {
-        setIsLoaded(true);
-        setData(result);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
-      });
-    }, [])
+// const URL_USER_DATA: string = 'https://aoe2.net/api/player/matches?game=aoe2de&steam_id=76561198134218675&count=1'
 
-    
-
-  if (error) {
-    return  <div> Error: {error.message} </div>;
-  } else if (!isLoaded) {
-    console.log("Loading");
-    return <div> loading... </div>;
-  } else {
-    console.log("Loaded");
-    if (data.length > 0) {
-      console.log(data[0].players);
-    }
-    return <div> Loaded </div>
+class UserInfo extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      steamID: '',
+      numGames: '',
+      games: [],
+    };
+  
+    // bind 'this' pointer to functions
+    this.handleSteamIDChange = this.handleSteamIDChange.bind(this);
+    this.handleNumGamesChange = this.handleNumGamesChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-}
 
-function GithubCommit() {
-  const [page, setPage] = useState(1);
-  const [commitHistory, setCommitHistory] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  handleSteamIDChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      steamID: event.target.value,
+    });
 
-  const loadMoreCommit = () => {
-    setPage(page + 1);
-  };
+  }
 
-  useEffect(() => {
-    fetch(
-      `https://api.github.com/search/commits?q=repo:facebook/react+css&page=${page}`,
-      {
-        method: "GET",
-        headers: new Headers({
-          Accept: "application/vnd.github.cloak-preview"
+  handleNumGamesChange(event) {
+    console.log(event.target.value);
+    this.setState({
+      numGames: event.target.value,
+    });
+  }
+
+  // Helper function for handle submit
+
+  handleSubmit(event) {
+    let apiUrl = 'https://aoe2.net/api/player/matches?game=aoe2de&steam_id=' + this.state.steamID + "&count=" + this.state.numGames;
+    console.log(apiUrl)
+
+    let data = undefined;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(result => {
+            data = _parseData(result, this.state.steamID);
+            this.setState({
+              games: _parseData(result, this.state.steamID),
+            });
         })
-      }
+        .catch(error => {
+          alert('Error: Check that steam id and number of games are correct.');
+          console.error('Error: ', error);
+        });
+
+    function _parseData(result, steamID) {
+        let civ = -1;
+        let map = -1;
+        let won = false;
+        let games = [];
+        result.forEach( (game: any) => {
+            // Parse data from api 
+            map = game.map_type;
+            for (let i = 0; i < game.players.length; i++) {
+                if (game.players[i].steam_id == steamID) {
+                    civ = game.players[i].civ;
+                    won = game.players[i].won;
+
+                    break;
+                }
+            }
+            // push data into games
+            games.push({
+                civ: civ,
+                map: map,
+                won: won,
+            });
+        });
+        return games;
+    }
+
+    this.setState({
+      games: data,
+    })
+    console.log(this.state.games);
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}> 
+          <div>
+            <label>
+              Steam ID: 
+              <input type="text" name="steam-id" value={this.state.steamID} onChange={this.handleSteamIDChange}/>
+            </label>
+          </div>
+          <div>
+            <label>
+              Number of games: 
+              <input type="text" name="game-count" value={this.state.numGames} onChange={this.handleNumGamesChange} />
+            </label>
+          </div>
+            <input type="submit" value="Submit"/>
+        </form>
+      </div>
     )
-      .then(res => res.json())
-      .then(response => {
-        setCommitHistory(response.items);
-        setIsLoading(false);
-      })
-      .catch(error => console.log(error));
-    }, [page]);
-
-
-  return (
-    <div>
-      <h1> API calls with React Hooks </h1>
-      {isLoading && <p>Wait I'm Loading comments for you</p>}
-
-      {commitHistory.length !== 0 && (
-        <button onClick={loadMoreCommit}>Load More Commits</button>
-      )}
-
-      {commitHistory.map((c, index) => (
-        <div key={index}>
-          {c.commit && (
-            <>
-              <div>
-                <h2 style={{ textDecoration: "Underline" }}>
-                  {c.commit.committer.name}
-                </h2>
-                <p>{c.commit.message}</p>
-              </div>
-              <hr />
-            </>
-          )}
-        </div>
-      ))}
-    </div>
-  );
+  }
 }
 
 /********************************************** FILTERS **************************************************/
@@ -144,7 +167,8 @@ function GithubCommit() {
 // }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<GetUserData />, rootElement);
+ReactDOM.render(<UserInfo />, rootElement);
+// ReactDOM.render(<GetUserData/>, rootElement);
 // ReactDOM.render(<GithubCommit />, rootElement);
 
 
